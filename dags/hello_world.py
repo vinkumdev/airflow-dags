9fe9916e45028ae32e790cda3704a97aeb9ee145
vinkumdev <vinkum099@gmail.com>
2025-06-01 11:07:52 +0100

    update

---
+++ dags/hello_world.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from datetime import datetime
+
+def hello_world():
+    print("Hello, Airflow 3!")
+
+dag = DAG(
+    dag_id="hello_world",
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    tags=["example"]
+)
+
+task = PythonOperator(
+    task_id="say_hello",
+    python_callable=hello_world,
+    dag=dag,
+)
