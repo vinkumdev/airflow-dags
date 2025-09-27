8a47dde1158536f6457d7038050fd2a37393bee2
vinkumdev <vinkum099@gmail.com>
2025-09-27 15:01:07 +0100

    update

---
+++ dags/load_price_paid_data.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime
+
+
+def query_postgres():
+    # Create Postgres connection (must exist in Airflow Connections UI as 'my_postgres')
+    hook = PostgresHook(postgres_conn_id="oxproperties_postgres")
+
+    # Run query
+    sql = "SELECT * FROM sample_data LIMIT 10;"
+    results = hook.get_records(sql)
+
+    # Print results
+    for row in results:
+        print(row)
+
+
+with DAG(
+        dag_id="postgres_query_example",
+        start_date=datetime(2023, 1, 1),
+        schedule_interval=None,  # Run manually
+        catchup=False,
+) as dag:
+    query_task = PythonOperator(
+        task_id="query_postgres_task",
+        python_callable=query_postgres,
+    )
